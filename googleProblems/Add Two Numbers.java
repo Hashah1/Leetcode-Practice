2bbbeaa0c662c817b56af08785a28b0bcf19fd8f
Mian Hashim Shah <mian_hashim_shah@hotmail.com>
2019-03-16 19:41:51 -0700

    Worked on addTwoNumbers.java.

--- googleProblems/Add Two Numbers.java
+++ googleProblems/Add Two Numbers.java
@@ -12,53 +12,57 @@ class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode iterate_l1 = l1;
         ListNode iterate_l2 = l2;
-        boolean is_l1_longer = false;
         int sum = 0;
-        do 
+        
+        while (iterate_l1.next != null && iterate_l2.next != null)
         {
-            // Calculate the sum of current node
-            sum = iterate_l1.val + iterate_l2.val;
-            System.out.println("Sum is: " + iterate_l1.val + " + " + iterate_l2.val + " = " + sum);
-            
-            // Pass sum to check if it has offset
-            if (isOffset(sum)){
-                // If there is an offset, add 1 to the
-                // next node's value and add 0 to current
-                if (iterate_l1.next != null && iterate_l2.next != null)
-                     iterate_l1.next.val += 1;
-                iterate_l1.val = 0;
-                //System.out.println(iterate_l1.next.val);
-            }
-            // Else, add the sum to l1's current node value
-            else{
-                iterate_l1.val = sum;
-            }
-            
-            //if (iterate_l1.next != null && iterate_l2.next != null){
-                // Move the two iterators to the next node
-                iterate_l1 = iterate_l1.next;
-                iterate_l2 = iterate_l2.next;
-            //}
-            
-            
-        } while (iterate_l1.next != null && iterate_l2.next != null);
-        // if l1 is longer, no need to copy
-        if (iterate_l1.next != null)
-        {
-            return l1;
-        }
+            calculateSum(sum, iterate_l1, iterate_l2);
+
+            // Move the two iterators to the next node
+            iterate_l1 = iterate_l1.next;
+            iterate_l2 = iterate_l2.next;
+        } 
+
+        calculateSum(sum, iterate_l1, iterate_l2);
+
         // if l2 is longer, merge the remainder characters to l1
-        else if (iterate_l2.next != null)
+        if (iterate_l2.next != null)
         {
-            iterate_l1.next = iterate_l2.next;
+            iterate_l1.next = l2;
         }
         return l1;
     }
-    
+
     // Helper function to check for offset of each sum
     boolean isOffset(int sum){
         if (sum > 9)
             return true;
         return false;
+    }
+    
+    void calculateSum(int sum, ListNode iterate_l1, ListNode iterate_l2)
+    {
+        // Calculate the sum of current node
+        sum = iterate_l1.val + iterate_l2.val;
+        System.out.println("Sum is: " + iterate_l1.val + " + " + iterate_l2.val + " = " + sum);
+
+        // Pass sum to check if it has offset
+        if (isOffset(sum)) {
+            // If there is an offset, add 1 to the
+            // next node's value and add 0 to current
+            if (iterate_l1.next != null && iterate_l2.next != null)
+                 iterate_l1.next.val += 1;
+            
+            // Check to see if there isnt anything next, add a new node to the end
+            else if (iterate_l1.next == null || iterate_l2.next == null) {
+                ListNode new_node = new ListNode(1);
+                iterate_l1.next = new_node;
+                }
+            iterate_l1.val = 0;
+        }
+        // Else, add the sum to l1's current node value
+        else {
+            iterate_l1.val = sum;
+        }
     }
 }
