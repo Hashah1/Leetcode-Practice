262589c9c39814d7fa8d89ff916a2d491863947d
Mian Hashim Shah <mian_hashim_shah@hotmail.com>
2019-07-10 17:30:57 -0700

    done more probzzzzzzzz

---
+++ easy/shortest_distance.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def shortestDistance(self, words, word1, word2):
+        """
+        :type words: List[str]
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        i1 = i2 = -1
+        min_dist = len(words)+1
+        for i, word in enumerate(words):
+            if word == word1:
+                i1 = i
+            elif word == word2:
+                i2 = i
+            if i1 != -1 and i2 != -1: 
+                min_dist = min(min_dist, abs(i1-i2))
+        return min_dist
+
+if __name__ == "__main__":
+    a = Solution()
+    a.shortestDistance(["a", "a", "b", "b"], "a", "b")
